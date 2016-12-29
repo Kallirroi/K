95084ad7a9e7f4248ca4c8733b27cfab0c099eca
Kallirroi <kallirroi.retzepi@gmail.com>
2016-12-28 23:56:09 -0500

    CSS

--- src/ItemMap.js
+++ src/ItemMap.js
@@ -1,22 +1,22 @@
 export const ItemMap = [
 	{
 		name: 'about',
-		text: '<p> My name is Kallirroi; many people call me Kalli. I explore the space found at the intersection of design, engineering, science and images.My focus lies in designing and developing novel platforms that breathe in data and exhale meaningful insights that inform decision-making on issues of collective learning, education and social awareness.</p>  <p> Currently, I am working as an interaction and data visualization designer at a digital design studio in Zurich, Switzerland. In my free time I run long distances, read, write, photograph, and have extensive, espresso-fueled debates - usually about politics.</p> '
+		text: '<p> My name is Kallirroi; many people call me Kalli. I explore the space found at the intersection of design, engineering, science and images.</p>  <p>In my free time I run long distances, read, write, photograph, and have extensive, espresso-fueled debates - usually about politics.</p> '
 	},
 	{
 		name: 'photography',
-		text: 'I use film (Fujicolor 200 is my go-to choice) and carry a small point-and-shoot Olympus XA in my bag. Instead of snapping a photo with my phone, I do it with my small film camera - a way to teach my eyes to not be so hungry to see the result right away.'
+		text: '<p>I use film (Fujicolor 200 is my go-to choice) and carry a small point-and-shoot Olympus XA in my bag. Instead of snapping a photo with my phone, I do it with my small film camera - a way to teach my eyes to not be so hungry to see the result right away.</p>'
 	},
 	{
 		name: 'thoughtless acts',
-		text: 'This is an ongoing exploration of the myriad of ways we inhabit the world around us - most often without noticing it. Inspired by the book of Jane Fulton Suri of IDEO titled "Thoughtless Acts", it is an exercise in seeing rather than looking at the different types of interactions we engage in with our surroundings.'
+		text: '<p>This project, inspired by the book of Jane Fulton Suri of IDEO titled "Thoughtless Acts", is an exercise in seeing rather than looking at the different types of interactions we engage in with our surroundings.</p>'
 	},
 	{
 		name: 'datavis + web design',
-		text: '<a href="http://cargocollective.com/kalliportfolio/" target="_blank">http://cargocollective.com/kalliportfolio/</a>'
+		text: '<p><a href="https://lab.interactivethings.com/road-to-school/" target="_blank">The road to school (link) </a> <br> an interactive visualization about how the Syrian conflict is stopping millions of children from going to school</p>'
 	},
 	{
-		name: 'contact me',
-		text: '<a href="mailto:kallirroi.retzepi@gmail.com">email</a> <br> <a href="https://twitter.com/kallirroi" target="_blank">twitter</a><br><a href="https://www.linkedin.com/in/kalliretzepi" target="_blank">linkedin</a>'
+		name: 'contact',
+		text: '<p><a href="mailto:kallirroi.retzepi@gmail.com">email</a> <br> <a href="https://twitter.com/kallirroi" target="_blank">twitter</a><br><a href="https://www.linkedin.com/in/kalliretzepi" target="_blank">linkedin</a><br><a href="https://kalli.ongoodbits.com/" target="_blank">newsletter</a></p>'
 	}
 ];	
