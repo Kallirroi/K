ea8a19ae99cfed698f4eb3536079465e4de266f7
Kallirroi <kallirroi.retzepi@gmail.com>
2016-12-28 22:51:25 -0500

    dangerously set inner html

--- src/ItemMap.js
+++ src/ItemMap.js
@@ -1,11 +1,11 @@
 export const ItemMap = [
 	{
 		name: 'about',
-		text: 'My name is Kallirroi; many people call me Kalli. I explore the space found at the intersection of design, engineering, science and images.My focus lies in designing and developing novel platforms that breathe in data and exhale meaningful insights that inform decision-making on issues of collective learning, education and social awareness. Having followed a non-linear career path, I gained experience both in research and in industry. Currently, I am working as an interaction and data visualization designer at a digital design studio in Zurich, Switzerland. Prior to that, I worked as a neuroscientist at Massachusetts General Hospital focusing on brain MRI analysis and visualization as well as clinical data collection, mining and interpretation. Simultaneously, I collaborated with a researcher at the Sabeti lab of The Broad Institute on UI design and implementation for a mobile app serving as the front-end of sophisticated Ebola virus risk prediction algorithms. In my free time I run long distances, read, write, photograph, and have extensive, espresso-fueled debates - usually about politics.'
+		text: 'My name is Kallirroi; many people call me Kalli. I explore the space found at the intersection of design, engineering, science and images.My focus lies in designing and developing novel platforms that breathe in data and exhale meaningful insights that inform decision-making on issues of collective learning, education and social awareness. Having followed a non-linear career path, I gained experience both in research and in industry. Currently, I am working as an interaction and data visualization designer at a digital design studio in Zurich, Switzerland. In my free time I run long distances, read, write, photograph, and have extensive, espresso-fueled debates - usually about politics.'
 	},
 	{
 		name: 'photography',
-		text: 'I use film (Fujicolor 200 is my go-to choice) and carry a small point-and-shoot Olympus XA in my bag. Instead of snapping a photo with my phone, I do it with my small film camera - a way to teach my eyes to not be so hungry to see the result right away. I have participated in many small-scale exhibitions in Europe and the USA, and my work has recently been featured in velveteyes and c41magazine.'
+		text: 'I use film (Fujicolor 200 is my go-to choice) and carry a small point-and-shoot Olympus XA in my bag. Instead of snapping a photo with my phone, I do it with my small film camera - a way to teach my eyes to not be so hungry to see the result right away.'
 	},
 	{
 		name: 'thoughtless acts',
@@ -13,7 +13,7 @@ export const ItemMap = [
 	},
 	{
 		name: 'datavis + web design',
-		text: 'http://cargocollective.com/kalliportfolio/'
+		text: '<a href="http://cargocollective.com/kalliportfolio/">http://cargocollective.com/kalliportfolio/</a>'
 	},
 	{
 		name: 'contact me',
