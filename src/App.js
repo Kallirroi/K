e1651b2e3ef86a085fef736596fc4d6284deeb48
Kallirroi <kallirroi.retzepi@gmail.com>
2017-01-01 12:10:17 -0500

    small CSS changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Item from './Item';
 import ItemDetails from './ItemDetails';
 import {ItemMap} from './ItemMap';
 
-const colors = ['#fff', '#F9FFF7', '#FAFFFE', '#FAFEFF', '#FEFEFF','#FCFFFA', '#FFFBFA', '#FFF'] ;
+// const colors = ['#fff', '#F9FFF7', '#FAFFFE', '#FAFEFF', '#FEFEFF','#FCFFFA', '#FFFBFA', '#FFF'] ;
+const colors = ['#FFF'] ;
 const background = document.getElementsByTagName('body');
 const item = document.getElementsByClassName("ItemDetails"); //change position
 
@@ -29,16 +30,18 @@ class App extends Component {
   }
   updateText(e) {
     if (e.target.id.length>0) this.setState({clickedID: e.target.id});  //change text
-    item[0].style.left = Math.random()*40 + 10 + 'rem';
-    item[0].style.top =  Math.random()*5 + 'rem';
-    background[0].style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+    item[0].style.left = Math.random()*40 + 10 + 'rem'; //move text 
+    item[0].style.top =  Math.random()*5 + 'rem'; //move text 
+    background[0].style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]; //change background color 
   }
 
   render() {
     return (
       <div className="App">
         <div className="App-header"> Kalli Retzepi </div> 
-        <div className="App-container"> {ItemMap.map((d,i) => <Item key={i} id={i} name={d.name} text={d.text} onClick={this.updateText} />)} </div>
+        <div className="App-container">
+          {ItemMap.map((d,i) => <Item key={i} id={i} name={d.name} text={d.text} onClick={this.updateText} />)} 
+        </div>
         <ItemDetails text={ItemMap[this.state.clickedID].text} />
       </div>
     );
--- src/ItemMap.js
+++ src/ItemMap.js
@@ -13,7 +13,7 @@ export const ItemMap = [
 	},
 	{
 		name: 'datavis + web design',
-		text: '<p><a href="https://lab.interactivethings.com/road-to-school/" target="_blank"> The road to school </a> <br> an interactive visualization about how the Syrian conflict is stopping millions of children from going to school</p> <p><a href="https://www.ubs.com/microsites/artificial-intelligence/en/new-dawn.html" target="_blank"> The evolution of AI </a> <br> a visual storytelling project about the future of artificial intelligence</p> <p><a href="http://kallirroi.github.io/dailyinstagram/" target="_blank"> Instagram panel </a> <br> my custom made Instagram stats overview</p> <p>(page is continuously updated)</p>'
+		text: '<p>– <br><a href="https://lab.interactivethings.com/road-to-school/" target="_blank"> The road to school </a> <br> an interactive visualization about how the Syrian conflict is stopping millions of children from going to school</p> <p> – <br><a href="https://www.ubs.com/microsites/artificial-intelligence/en/new-dawn.html" target="_blank"> The evolution of AI </a> <br> a visual storytelling project about the future of artificial intelligence</p> <p>– <br><a href="http://kallirroi.github.io/dailyinstagram/" target="_blank"> Instagram panel </a> <br> my custom made Instagram stats overview</p> <br><p>(page is continuously updated)</p>'
 	},
 	{
 		name: 'creative code',
