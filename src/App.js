e77d911eeb83110af7c89960bd8e0a453a97f6a6
Kallirroi <kallirroi.retzepi@gmail.com>
2017-01-28 00:23:13 -0500

    added Draggable, corrected onClick() behavior

--- src/App.js
+++ src/App.js
@@ -5,31 +5,13 @@ import Item from './Item';
 import ItemDetails from './ItemDetails';
 import {ItemMap} from './ItemMap';
 
-// const colors = ['#fff', '#F9FFF7', '#FAFFFE', '#FAFEFF', '#FEFEFF','#FCFFFA', '#FFFBFA', '#FFF'] ;
-const colors = ['#FFF'] ;
-const item = document.getElementsByClassName("ItemDetails"); //change position
 
 class App extends Component {
-  
-  constructor(props) {
+constructor(props) {
     super(props);
     this.state = {
       clickedID: 0
     };
-    this.updateText = this.updateText.bind(this);
-  }
- 
-  componentDidMount() {
-    window.addEventListener('click', this.updateText);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('click', this.updateText);
-  }
-  updateText(e) {
-    if (e.target.id.length>0) this.setState({clickedID: e.target.id});  //change text
-    item[0].style.left = Math.random()*40 + 20 + 'rem'; //move text 
-    item[0].style.top =  Math.random()*10 + 10 + 'rem'; //move text 
   }
 
   render() {
@@ -37,7 +19,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header"> Kalli Retzepi </div> 
         <div className="App-container">
-          {ItemMap.map((d,i) => <Item key={i} id={i} name={d.name} text={d.text} onClick={this.updateText} />)} 
+          {ItemMap.map((d,i) => <Item key={i} id={i} name={d.name} text={d.text} />)} 
         </div>
         <ItemDetails text={ItemMap[this.state.clickedID].text} />
         <div className="footer">made by K. – 2017</div>
--- src/ItemDetails.js
+++ src/ItemDetails.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Draggable from 'react-draggable'; 
 
 class ItemDetails extends React.Component {
   render() {
     return (
-	  		<div className="ItemDetails" dangerouslySetInnerHTML={ {__html: this.props.text} }></div>
+    		<Draggable>
+	  			<div className="ItemDetails" dangerouslySetInnerHTML={ {__html: this.props.text} }></div>
+	  		</Draggable>
     );
   }
 }
