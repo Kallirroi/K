7c556329be92e233d072835d32aa60522a28cdf3
Kallirroi <kallirroi.retzepi@gmail.com>
2017-03-17 13:09:55 -0400

    images ready

--- src/ItemPhoto.js
+++ src/ItemPhoto.js
@@ -4,7 +4,7 @@ class ItemPhoto extends React.Component {
 
 render() {
 	return (
-		<div className={this.props.className} id={this.props.id} dangerouslySetInnerHTML={{__html: this.props.photos}}></div>
+		<div className={this.props.className} id={this.props.id}><img src={require(this.props.photos)} /></div>
 	);
 	}
 }
--- src/ItemPhotoMap.js
+++ src/ItemPhotoMap.js
@@ -1,64 +1,64 @@
 export const ItemPhotoMap = [
 	{
 		header: 'The road to school',
-		photos: '<img src="http://payload492.cargocollective.com/1/22/719859/12153399/design_670.png">'
+		photos: './images/roadtoschool.png'
 	},
 	{
 		header: 'Film photography',
-		photos: '<img src="http://payload359.cargocollective.com/1/18/596066/9499254/A184498_006A_670.jpg">'
+		photos: './images/photo.png'
 
 	},
 	{
 		header: 'I just can‘t decide',
-		photos: '<img src="https://raw.githubusercontent.com/Kallirroi/K/master/screegrabs/030317.png">'
+		photos: './images/K.png'
 	},
 	{
 		header: 'samtous.wtf',
-		photos: '<img src="http://payload359.cargocollective.com/1/18/596066/9499254/A184498_006A_670.jpg">'
+		photos: './images/samtous.png'
 
 	},
 	{
 		header: 'archmix.es ',
-		photos: '<img src="http://payload359.cargocollective.com/1/18/596066/9499254/A184498_006A_670.jpg">'
+		photos: './images/archmixes.png'
 
 	},
 	{
 		header: 'Responsive d3.js charts library',
-		photos: '<img src="http://payload492.cargocollective.com/1/22/719859/12153580/ezgif-1087629858.gif">'
+		photos: './images/SSZ.png'
 
 	},	
 	{
 		header: 'EBOLA risk prediction app',
-		photos: '<img src="http://payload492.cargocollective.com/1/22/719859/12141099/refinement_670.png">'
+		photos: './images/Ebola.png'
 
 	},
 	{
 		header: 'Thoughtless acts',
-		photos: '<img src="http://payload479.cargocollective.com/1/18/596066/11893936/thoughtlessacts_book_07_670.jpg">'
+		photos: './images/ThoughtlessActs.png'
 	},	
 	{
 		header: 'Master‘s thesis (2014)',
-		photos: '<img src="http://payload492.cargocollective.com/1/22/719859/12141104/brains_670.png">'
+		photos: './images/Masters.png'
 
 	},
 	{
 		header: 'Birthday cake robot ',
-		photos: '<img src="http://payload492.cargocollective.com/1/22/719859/12154781/apps_670.png">'
+		photos: './images/robot.png'
 
 	},
 	{
 		header: 'The evolution of AI',
-		photos: '<img src="http://payload492.cargocollective.com/1/22/719859/12141107/published_670.png">'
+		photos: './images/UBS.png'
 
 	},
 	{
 		header: 'EXPLORA',
-		photos: '<img src="http://payload359.cargocollective.com/1/18/596066/9499254/A184498_006A_670.jpg">'
+		photos: './images/EXPLORA.png'
 
 	},
 	{
 		header: 'p5.js sketches',
-		photos: '<img src="http://payload494.cargocollective.com/1/22/719859/12196432/1_670.png">'
+		photos: './images/p5.js.png'
 
 	}
 ];	
