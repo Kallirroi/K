4af2356fca21954dcf8873d7763225dce3de5d74
Kallirroi <kallirroi.retzepi@gmail.com>
2017-04-29 21:25:08 -0400

    design changes

--- src/ItemMap.js
+++ src/ItemMap.js
@@ -1,7 +1,7 @@
 export const ItemMap = [
 	{
 		header: 'The road to school',
-		type: 'datavis + storytelling',
+		type: 'datavis  •  2016',
 		text: 'I believe that education and literacy are the most important tools each person in our world can have, and it is currently being taken away from millions of Syrian children. This data visualization and storytelling experience addresses this issue and aims to sensitize the audience to it. ',
 		links:'<a href="https://lab.interactivethings.com/road-to-school/" target="_blank">link</a>',
 		photos: 'image'
@@ -15,15 +15,8 @@ export const ItemMap = [
 
 	},
 	{
-		header: 'I just can‘t decide',
-		type: 'web design + dev',
-		text: '...how my website should look. It is something that varies with my mood, the seasons, what I am working on, and what web technology I want to experiment with. I decided to dedicate some space on my website in order to archive the previous versions.',
-		links:'<a href="http://samtous.wtf/" target="_blank">link</a>',
-		photos: 'image'
-	},
-	{
 		header: 'samtous.wtf',
-		type: 'web design + dev',
+		type: 'design/dev  •  2017',
 		text: 'The personal website of Sam Ghantous serves as an archive of work across different mediums, contexts, and depths. This space is a collection of content that is active; it accumulates and gets agitated, challenging its visitors‘ attention and its own stability.',
 		links:'<a href="http://samtous.wtf/" target="_blank">link</a>',
 		photos: 'image'
@@ -31,7 +24,7 @@ export const ItemMap = [
 	},
 	{
 		header: 'archmix.es ',
-		type: 'web design + dev',
+		type: 'design/dev  •  2017',
 		text: 'This website serves as a navigation page for a project about architecture and its circulation online. It revolves around a Twitter bot that anyone can use to make architectural mashups, that then get sent to different places around the web variously as images, 3d models, and sales listings.',
 		links:'<a href="http://archmix.es/" target="_blank"> link</a>',
 		photos: 'image'
@@ -39,7 +32,7 @@ export const ItemMap = [
 	},
 	{
 		header: 'Responsive d3.js charts library',
-		type: 'web dev',
+		type: 'dev  •  2016',
 		text: 'While I was working at IXT, I contributed to the making of a Javascript library that facilitates the creation of d3.js data visualizations for the City of Zurich official website, using the reusable chart pattern.',
 		links:'<a href="https://www.stadt-zuerich.ch/content/prd/de/index/statistik/publikationen-angebote/publikationen/webartikel/2016-02-23_Wohnbautaetigkeit-auf-Rekordniveau.html" target="_blank">link #1</a> • <a href="https://www.stadt-zuerich.ch/content/prd/de/index/statistik/publikationen-angebote/publikationen/webartikel/2015-12-17_Wohneigentum-in-der-Stadt-Zuerich.html" target="_blank">link #2</a>',
 		photos: 'image'
@@ -47,7 +40,7 @@ export const ItemMap = [
 	},	
 	{
 		header: 'EBOLA risk prediction app',
-		type: 'UI/UX design',
+		type: 'UI/UX  •  2014',
 		text: 'I volunteered to create the UI design as well as the Android implementation of a mobile app that packages the best models of a machine learning pipeline for Ebola Virus Disease (EVD) prognosis predictions. The risk prediction algorithms were already developed by the project‘s leader <a href="https://www.sabetilab.org/andres-colubri/" target="_blank">(Andres Colubri)</a>.',
 		links:'<a href="http://journals.plos.org/plosntds/article?id=10.1371/journal.pntd.0004549" target="_blank">link</a>',
 		photos: 'image'
@@ -55,15 +48,15 @@ export const ItemMap = [
 	},
 	{
 		header: 'Thoughtless acts',
-		type: 'design thinking',
+		type: 'personal  •  2016',
 		text: 'This is an ongoing exploration of the myriad of ways we inhabit the world around us - most often without noticing it. Inspired by the book of Jane Fulton Suri of IDEO titled <a href="https://www.ideo.com/post/thoughtless-acts" target="_blank">"Thoughtless Acts"</a>, it is an exercise in seeing rather than looking at the different types of interactions we engage in with our surroundings. ',
 		links:'<a href="http://kalli.photography/thoughtless-acts" target="_blank">link</a>',
 		photos: 'image'
 
 	},	
 	{
-		header: 'Master‘s thesis (2014)',
-		type: 'brain MR imaging research',
+		header: 'Master‘s thesis in Neuroscience',
+		type: 'research  •  2013',
 		text: ' My Master thesis demonstrates the feasibility of identifying, retrieving and analyzing for research purposes data from clinical MRI scans acquired in patients with chronic focal pain diagnoses. A computerized brain model as well as morphometric indices were generated for each patient.',
 		links:'',
 		photos: 'image'
@@ -71,7 +64,7 @@ export const ItemMap = [
 	},
 	{
 		header: 'Birthday cake robot ',
-		type: 'web app',
+		type: 'dev  •  2016',
 		text: ' A friend designed a cake decoration machine that sprays edible food dye onto a cake, and demo-ed it at a birthday party. I created the front and back-end of a web application that collected birthday messages which were then spray painted on the cake‘s frosting, in real time.',
 		links:'',
 		photos: 'image'
@@ -79,7 +72,7 @@ export const ItemMap = [
 	},
 	{
 		header: 'The evolution of AI',
-		type: 'design',
+		type: 'design  •  2016',
 		text: 'The scope of this project was to support UBS publishing a multimedia, longform article. Its content is based on the previously published article of the T Brand Studio in the New York Times titled <a href="http://paidpost.nytimes.com/ubs/what-it-takes-to-be-human.html">"What it takes to be human"</a>.',
 		links:'<a href="https://www.ubs.com/microsites/artificial-intelligence/en/new-dawn.html" target="_blank"> link</a>',
 		photos: 'image'
@@ -87,7 +80,7 @@ export const ItemMap = [
 	},
 	{
 		header: 'EXPLORA',
-		type: 'design',
+		type: 'design  •  2016',
 		text: 'A collaboration for a visual storytelling project on behalf of the ETH Library. My tasks on this project involved mostly design and curation of the stories to be published on the online platform. A mobile-first approach was tricky to nail but eventually did.',
 		links:'<a href="https://www.explora.ethz.ch/" target="_blank"> link</a>',
 		photos: 'image'
@@ -95,7 +88,7 @@ export const ItemMap = [
 	},
 	{
 		header: 'p5.js sketches',
-		type: 'creative coding',
+		type: 'creative code',
 		text: 'My experiments with the javascript version of Processing. What I like with p5.js is how easy it is to iterate and explore different concepts. The code structure is similar to Processing (two main functions that wrap all action) which definitely has its pitfalls but I enjoy the overall playfulness and definitely the online community.',
 		links:'<a href="https://github.com/Kallirroi/p5" target="_blank">link</a>',
 		photos: 'image'
